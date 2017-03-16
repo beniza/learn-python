988d039afc831bc63a13f2757c9709d8e72c9cb8
Bijo Babu <bijob89@gmail.com>
2017-03-16 17:06:53 +0530

    Addition functionality updated

--- myCalc.py
+++ myCalc.py
@@ -1,10 +1,11 @@
 # Calculator project for collaboration
-a = raw_input("Enter the first number :")
-b = raw_input("\nEnter the second Number :")
-print "The sum is :", a + b
+def addition(x, y):
+	return a + b
 
 def division(x, y):
 	if(y!=0):
 		return(x/y)
 	else:
 		return("Division by Zero not defined")
+
+
