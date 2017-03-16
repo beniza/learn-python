cb0811ae351577d39cf4370c7b210ccfb0d4bca9
benVar <beniza@gmail.com>
2017-03-16 16:02:34 +0530

    resolved conflicts between bijo branch

--- myCalc.py
+++ myCalc.py
@@ -2,3 +2,9 @@
 a = raw_input("Enter the first number :")
 b = raw_input("\nEnter the second Number :")
 print "The sum is :", a + b
+
+def division(x, y):
+	if(y!=0):
+		return(x/y)
+	else:
+		return("Division by Zero not defined")
