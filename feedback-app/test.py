d58e5d7580291d6a9d571d945992683b0f53e8bb
Beniza <beniza@gmail.com>
2020-04-25 23:44:15 +0530

    initiate folder for CS50 learning

---
+++ feedback-app/test.py
@@ -0,0 +1,5 @@
+gibwords = ['Egbindinatl', 'Evtlesnioi', 'Miet', 'Boilme', 'Ishtr', 'Agdrne', 'Solhoc', 'sjdmai', 'Tsere', 'Yclecs', 'Neplci', 'Odosretp', 'Thgli', 'Sglas', 'Owilpl']
+for gibword in gibwords:
+  gibword = gibword.lower()
+  l = [l for l in gibword]
+  print(l)
